559ca71a7a254b43c3a0adc74be1a35d3085b3b8
Dustin Li <dustin.li1988@gmail.com>
2022-07-31 09:58:46 +0000

    Add company funtionality and page

--- workflowmanager/routes.py
+++ workflowmanager/routes.py
@@ -1,7 +1,27 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from workflowmanager import app, db
 from workflowmanager.models import Company, Task
 
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+@app.route("/companies")
+def companies():
+    return render_template("companies.html")
+
+@app.route("/add_company", methods=["GET", "POST"])
+def add_company():
+    companies = list(Company.query.order_by(Company.company_name).all())
+    if request.method == "POST":
+        company = Company(
+            company_name=request.form.get("company_name"),
+            contact_no=request.form.get("contact_no"),
+            email=request.form.get("email"),
+            address=request.form.get("address"),
+        )
+        db.session.add(company)
+        db.session.commit()
+        flash("Company Successfully added", category='success')
+        return redirect(url_for("add_company"))
+    return render_template("companies.html", companies=companies)
